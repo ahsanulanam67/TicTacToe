bbf8079a948d339b068f6753e31fff4dbdc4b19d
Ahsanul_Anam <ahsanulanamsaboj1999@gmail.com>
2025-04-25 02:02:21 +0600

    Time travel array of array added

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,34 +1,84 @@
 import Squares from "./Squares";
-import {useState} from 'react';
+// import {useState} from 'react';
 
-export default function Board() {
+export default function Board({squares, nextTurn, onPlayReference}) {
 
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [nextTurn,setnextTuren] = useState(0);
+  
 
-    function handClick(){
+    function handClick(ind){
 
+      if(squares[ind] || winner)return;
+        const nextSquares = squares.slice();
+
+        if(nextTurn){
+
+          nextSquares[ind] = 'X';
+        }
+        else {
+          nextSquares[ind] = 'O';
+        }
+        
+        onPlayReference(nextSquares);
 
     }
+
+    const winner = whoisthewinner(squares);
+
+    let status;
+
+    if(winner){
+      status = "Winner is " + winner;
+    }
+    else status = "Next turn is for : " + (nextTurn ? 'X':'O');
+
   return (
     <>
       <h1>This is the TicTacToe Board</h1>
       <div className="board-row">
-        <Squares value={squares[0]} onClickReference={handClick}/>
-        <Squares value = {squares[1]} onClickReference={handClick} />
-        <Squares value={squares[2]} onClickReference={handClick}/>
+        <Squares value={squares[0]} onClickReference={()=>handClick(0)}/>
+        <Squares value = {squares[1]} onClickReference={() => handClick(1)} />
+        <Squares value={squares[2]} onClickReference={() => handClick(2)}/>
       </div>
       <div className="board-row">
-        <Squares value = {squares[3]} onClickReference={handClick} />
-        <Squares value = {squares[4]} onClickReference={handClick}/>
-        <Squares value = {squares[5]} onClickReference={handClick}/>
+        <Squares value = {squares[3]} onClickReference={() => handClick(3)} />
+        <Squares value = {squares[4]} onClickReference={() => handClick(4)}/>
+        <Squares value = {squares[5]} onClickReference={() => handClick(5)}/>
 
       </div>
       <div className="board-row">       
-        <Squares value = {squares[6]} onClickReference={handClick}/>
-        <Squares value = {squares[7]} onClickReference={handClick}/>
-        <Squares value = {squares[8]} onClickReference={handClick}/>
+        <Squares value = {squares[6]} onClickReference={() => handClick(6)}/>
+        <Squares value = {squares[7]} onClickReference={() => handClick(7)}/>
+        <Squares value = {squares[8]} onClickReference={() => handClick(8)}/>
       </div>
+      <div>
+
+        <p>{status}</p>
+      </div>
+      
     </>
   );
+}
+
+function whoisthewinner(squares){
+
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for(let i=0;i<lines.length;i++){
+
+      const [a,b,c] = lines[i];
+
+      if(squares[a] && squares[a]===squares[b] && squares[b] === squares[c])return squares[a];
+      
+  }
+
+
 }
--- src/Components/TicTacToe.js
+++ src/Components/TicTacToe.js
@@ -1,13 +1,22 @@
 import Board from "./Board";
-
+import {useState} from "react";
 export default function TicTacToe(){
 
+    const [allChangedSquares, setallChangedSquares] = useState([Array(9).fill(null)]);
+    const [nextTurn,setnextTurn] = useState(0);
+    const currentSquares = allChangedSquares[allChangedSquares.length-1];
 
+    function onPlay(nextSquares){
+
+        setallChangedSquares([...allChangedSquares, nextSquares]);
+        setnextTurn(!nextTurn);
+
+    }
     return (
         <>
         <div className="game">
             <p>Ahsanul Anam</p>
-            <Board/>
+            <Board squares={currentSquares} nextTurn={nextTurn}  onPlayReference ={onPlay}  />
         </div>
         </>
     );
